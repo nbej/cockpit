56dfdddfd4ca5e99bf4fe4a9ad0139aea2f67b68
nbej <78787157+nbej@users.noreply.github.com>
2021-02-15 21:46:40 +0300

    Delete pkg directory

--- pkg/kdump/crashkernel.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -eu
-
-# sync disks
-sync
-
-# crash the kernel
-echo 1 > /proc/sys/kernel/sysrq
-echo c > /proc/sysrq-trigger
--- pkg/kdump/testwritable.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -eu
-
-tmpfile=$(mktemp $1/.XXXXXX)
-rm "$tmpfile"
